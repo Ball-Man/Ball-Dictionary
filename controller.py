0ea6c113c2ee78ac56b3dcebec30c5892b32b070
Francesco Mistri <francesco.mistri98@gmail.com>
2019-11-29 19:10:06 +0100

    Add, remove and search for entries
    It's now possible to add, remove and search for entries. The search
    method searches for entries similar to a given keyword. It searches
    using a comparing algorithm which returns a similarity ratio. The search
    is done with a threshold similarity ratio.

--- controller.py
+++ controller.py
@@ -2,6 +2,17 @@
 import os
 import os.path as op
 import json
+from difflib import SequenceMatcher
+
+
+# Constants
+WORD = 0
+TRANS = 1
+
+
+def _similar(a, b):
+    """Find the similarity between two strings."""
+    return SequenceMatcher(None, a, b).ratio()
 
 
 class Controller:
@@ -10,6 +21,7 @@ class Controller:
     This class acts as a logical controller, loading/saving changes
     to/from the dictionary.
     """
+
     def __init__(self, dict_path):
         """Create a new controller, based on a dictionary path.
 
@@ -17,6 +29,7 @@ class Controller:
         created.
         """
         self._dictionary_path = None
+        # It's called dictionary but in reality it's a list
         self._dictionary = None
 
         self.load(dict_path)
@@ -30,6 +43,8 @@ class Controller:
         if op.isfile(path):
             with open(path) as file:
                 self._dictionary = json.load(file)
+        else:
+            self._dictionary = []
 
         self._dictionary_path = path
 
@@ -50,3 +65,64 @@ class Controller:
 
         with open(path, 'w') as file:
             json.dump(self._dictionary, file)
+
+    def add_entry(self, word, trans):
+        """Add an entry in the dict.
+
+        word is the new word you want to remember, trans is the
+        translation in your main language.
+        """
+        self._dictionary.append([word, trans])
+
+    def remove_entry(self, word):
+        """Remove the word entry from the dictionary."""
+        i = 0
+        remove_ind = i
+        # Find the given word in the dictionary
+        for w, t in self._dictionary:
+            if w == word:
+                remove_ind = i
+
+            i += 1
+
+        # If the word was found, remove its entry
+        if remove_ind < len(self._dictionary):
+            del self._dictionary[remove_ind]
+
+    def search_entries(self, keyword=None, threshold=0):
+        """Search entries.
+
+        keyword will be used as search parameter. If the similarity
+        between keyword and w or t(from the entry pair (w, t)) is
+        greater than the given threshold, the selected pair will be
+        added to the output.
+
+        Return a list of pairs (w, t) matching the research. If keyword
+        is None return all the entries.
+        The entries are sorted so that the higher similarities come
+        first.
+        """
+
+        def _max_ratio(kw, entry):
+            """Return the maximum similarity ratio.
+
+            The ratio is calculated between the keyword and w, and
+            between the keyword and t. The maximum is returned.
+            """
+            return max(_similar(keyword, entry[WORD]),
+                       _similar(keyword, entry[TRANS]))
+
+        if keyword is None:
+            keyword = ''
+
+        # Obtain a tuple structured as follows: ( (ratio, w, t), * )
+        ratios_dict = map(lambda x: (_max_ratio(keyword, x), x),
+                          self._dictionary)
+        filtered_ratios = filter(lambda x: x[0] >= threshold, ratios_dict)
+        sorted_ratios = sorted(filtered_ratios, key=lambda x: x[0],
+                               reverse=True)
+
+        # Go back to the canonical entry format
+        entries = map(lambda x: x[1], sorted_ratios)
+
+        return tuple(entries)
--- main.py
+++ main.py
@@ -4,7 +4,10 @@ import controller
 
 def main():
     c = controller.Controller('t/test.json')
-    c.save()
+    c.add_entry('chico', 'ragazzo')
+    c.add_entry('manzana', 'mela')
+    print(c.search_entries())
+    print(c.search_entries('manana', 0.6))
 
 
 if __name__ == '__main__':
